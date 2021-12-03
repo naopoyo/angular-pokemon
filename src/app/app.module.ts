edf21bfffc4684ec20378357f916decd69f3e5af
naopoyo <5816375+naopoyo@users.noreply.github.com>
2021-12-03 22:30:14 +0900

    [Part3] graphql.module.tsを移動

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
+import { GraphQLModule } from './graphql/graphql.module';
 
 @NgModule({
   declarations: [
@@ -12,9 +12,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     AppRoutingModule,
     GraphQLModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
