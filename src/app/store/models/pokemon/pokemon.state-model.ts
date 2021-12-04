527634077dfb3050ee8b069c753772d4b8f3e958
naopoyo <5816375+naopoyo@users.noreply.github.com>
2021-12-04 13:58:22 +0900

    [Part4] ストア用のポケモンモデル作成

---
+++ src/app/store/models/pokemon/pokemon.state-model.ts
@@ -0,0 +1,40 @@
+export interface PokemonStateModel {
+  id: string
+  number?: string
+  name?: string
+  weight?: {
+    minimum: string
+    maximum: string
+  }
+  height?: {
+    minimum: string
+    maximum: string
+  }
+  classification?: string
+  types?: string
+  resistant: string[]
+  attacks: {
+    fast: {
+      name: string
+      type: string
+      damage: number
+    }[]
+    special: {
+      name: string
+      type: string
+      damage:number
+    }[]
+  }
+  weaknesses: string[]
+  fleeRate?: number
+  maxCP?: number
+  evolutions: {
+    id: string
+  }[]
+  evolutionRequirements?: {
+    amount: number
+    name: string
+  }
+  maxHP?: number
+  image?: string
+}
